765483949f2c5014052e9b8337a2621831067e2c
Michael Ihde <michael.ihde@lst.team>
2025-02-13 13:09:23 +0100

    create basic structure for persons crud

---
+++ README.md
@@ -0,0 +1,5 @@
+This App demonstrates Symfony UX with Stimulus Controllers, Turbo Streams and Twig/LiveComponents. The App has 3 Parts to demonstrate the different UX Bundles.
+
+1. Persons CRUD: This is the "Stimulus Controller" (Vanilla JS/AJAX) Version.
+2. Tasks CRUD: This is the "Turbo" Version.
+3. **TBD**: Is the "TwigComponents"/"LiveComponents" Version.
--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,8 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import '../assets/vendor/bootstrap/bootstrap.index.js';
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import * as bootstrap from "bootstrap";
